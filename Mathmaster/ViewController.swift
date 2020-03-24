db4d2f203acc128f6e9391686f14ed2f7e03c802
keval dattani <kdattani07@gmail.com>
2020-03-24 11:29:00 +0530

    Initial Commit

---
+++ Mathmaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mathmaster
+//
+//  Created by keval dattani on 24/03/20.
+//  Copyright © 2020 keval dattani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
